f637169f9088656bb4717bd2faee2883986ff057
23b2119 <167267778+23b2119@users.noreply.github.com>
2024-07-10 12:09:53 +0900

    Update and rename let.py to 標準入力.js

--- let.py
+++
@@ -1,8 +0,0 @@
-/*
-let 変数名 = "文字列";
-console.log(変数名);
-再宣言はできない
-*/
-
-let greeting = "Hello world";
-console.log(greeting);
---
+++ 標準入力.js
@@ -0,0 +1,6 @@
+// データを受け取って表示しよう
+let lines = require("fs").readFileSync("/dev/stdin", "utf8").split("\n");
+let message = lines[0];
+console.log(message);
+<<<paizaラーニング
+>>>paizaラーニング
